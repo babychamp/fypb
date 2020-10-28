1220a8441a83dcc3a07c70da747bb62fab45fb96
Alif414 <Alif414@users.noreply.github.com>
2020-10-28 16:30:00 +0800

    register.php update
    Removed unused code

--- fyp/register.php
+++ fyp/register.php
@@ -55,62 +55,6 @@
             
    echo' </header>';
 
-if (isset($_POST['register-submit'])){
-	
-	$id = $_POST['id'];
-	$type = $_POST['type'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$passwordRpt = $_POST['pwd-repeat'];
-    $email = $_POST['email'];
-	$type = 2;
-	
-	$sql = "SELECT email FROM users WHERE email=?";
-	$stmt = mysqli_stmt_init($conn); // check if we can prepared the statement and connection
-	if(!mysqli_stmt_prepare($stmt, $sql)){
-		echo '<script type="text/javascript">
-						alert("SQL error.");
-					</script>';
-    }else{
-		mysqli_stmt_bind_param($stmt, "s",$email);
-		mysqli_stmt_execute($stmt);
-		mysqli_stmt_store_result($stmt);
-		$resultCheck = mysqli_stmt_num_rows($stmt); // check whether email exist or not
-		if($resultCheck > 0){
-        echo '<script type="text/javascript">
-						alert("Email is being taken or registered.");
-					</script>';
-		}else{
-			if ($password !== $passwordRpt){
-				echo '<script type="text/javascript">
-						alert("Passwords do not match. Please try again.");
-					</script>';
-			}else{
-				$sql = "INSERT INTO users (id, type, username, password, email) VALUES (?, ?, ?, ?, ?)";
-			$stmt = mysqli_stmt_init($conn);
-			if(!mysqli_stmt_prepare($stmt, $sql)){
-				echo '<script type="text/javascript">
-						alert("SQL error.");
-					</script>';
-			}else{
-				// hash the password using bcrypt
-				$hashPwd = password_hash($password, PASSWORD_DEFAULT);
-				mysqli_stmt_bind_param($stmt, "issss",$id, $type, $username, $password, $email);
-				mysqli_stmt_execute($stmt);
-				echo '<script type="text/javascript">
-					alert("Register Successful");
-					window.location = "login.php";
-				</script>';
-				exit();
-				}
-			}	
-		}
-	}
-	mysqli_stmt_close($stmt); // close the statement to save resources
-	mysqli_close($conn);  // close the connection to save resources
-}
-?>
-
 <div class="col-md-12">';
                <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
         
